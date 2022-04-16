f4b719a154e89e6397f69e89a5536dd026a7240e
luixfds <l.fds@yahoo.com>
2022-04-16 02:52:15 -0300

    home pt1 responsive

---
+++ Frontend/pages/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <div class="content">
+          <div class="img-logo"><img src="./../assets/media/animeflix-logo.png" alt=""></div>
+            <div class="spotlight">
+                <div class="left">
+                    <img class="title" src="./../assets/media/animeflix-logo.png">
+                    <div class="details">
+                        <label>Lançamento 2016</label>
+                        <label>2 Temporadas</label>
+                    </div>
+                    <div class="sinopse">
+                        <label>Naruto é um jovem órfão habitante da Vila da Folha que sonha se tornar o quinto Hokage, o maior guerreiro e governante da vila. Ao se graduar como ninja, descobre que tem um demônio raposa selado dent</label>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="blur"></div>
+                </div>
+            </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    @import "./../assets/scss/pages/home.scss"
+</style>
